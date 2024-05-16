56a17d61369d63f53a942a11a4c113d9abc8b21a
VoytovychM <mary.voytovych@gmail.com>
2024-05-16 12:46:51 +0200

    add file
    task1

--- VoytovychM/task1.sql
+++ VoytovychM/task1.sql
@@ -1,5 +1,17 @@
--- Вывести общее количество клиентов не из Germany --
-SELECT COUNT(*) AS total_clients_not_from_Germany
-FROM Customers
-WHERE 
-NOT Country = 'Germany'
+CREATE TABLE transactions (
+	id int primary key auto_increment, 
+	created_at timestamp not NULL default current_timestamp, 
+	sender_account_id int not null, 
+	recipient_account_id int not null, 
+	amount float not null, 
+	refernce_of_transaction varchar(240) not null,
+
+	foreign key(sender_account_id) references accounts(id),
+	foreign key(recipient_account_id) references accounts(id)
+);
+
+INSERT INTO transactions (sender_account_id, recipient_account_id, amount, refernce_of_transaction)
+VALUES
+	(3, 4, 1500.00, 'Transfer of personal funds'),
+	(3, 4, 100.00, 'Payment for the goods'),
+	(4, 3, 200.00, 'Transfer of personal funds');
